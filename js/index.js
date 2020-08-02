10bda9bc44aca53153f8a109f577f86d7cefa497
Nev-aA <alexandre.gasior@gmail.com>
2020-08-02 15:14:08 +0200

    index page done apart from eventListener on btn click

--- js/index.js
+++ js/index.js
@@ -1,15 +1,24 @@
+// get every teddies from backend to diplay them on front page
 fetch('http://localhost:3000/api/teddies')
   .then(response => response.json())
   .then(teddies => teddies.forEach(teddy => {
-    const selectMain = document.getElementsByTagName('main')[0];
-    console.log(teddy);
-    selectMain.innerHTML +=`
-    <div class="card col-12 col-md-6 col-lg-4">
-        <img class="card-img-top" src="${teddy.imageUrl}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">${teddy.name}</h5>
-            <p class="card-text">${teddy.description}</p>
-            <a href="product.html" class="btn btn-primary">En savoir plus</a>
+    const selectMainRow = document.getElementById('productsDisplay');
+    selectMainRow.innerHTML +=`
+      <div class="col-12 col-md-6 col-lg-4 p-2">
+        <div class="card text-center bg-primary" style="width: 18rem;">
+          <img class="card-img-top" src="${teddy.imageUrl}" style="max-height: 12rem;" alt="Un adorable ours en peluche">
+          <div class="card-body">
+              <h3 class="card-title">${teddy.name}</h3>
+              <p class="card-text">${teddy.description}</p>
+              <a href="products.html" class="btn btn-info" id="${teddy._id}">Commander - ${teddy.price/100} €</a>
+          </div>  
         </div>
-    </div>`
+      </div>`
   }))
+
+// // listen event on btn so that id is given to the next page
+// const buttons = document.getElementsByClassName("btn");
+// buttons.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   console.log('Ok !!')
+// });
