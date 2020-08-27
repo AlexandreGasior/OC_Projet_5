0f30e084b7eac60b41d3ede7f2a5a3bf48f99f08
Nev-aA <alexandre.gasior@gmail.com>
2020-08-28 01:30:00 +0200

    functionnal version done

--- js/basket.js
+++ js/basket.js
@@ -1,7 +1,8 @@
 const selectProductsDisplay = document.getElementById('productsDisplay');
-const selectForm = document.getElementById('order-form');
-const selectFormButton = document.getElementById('order-button');
+const selectFormContainer = document.getElementById('order-form');
+const selectForm = document.querySelector('form');
 const myBasket = JSON.parse(localStorage.getItem('myBasket'));
+let totalPrice = 0;
 
 // check if localStorage contains items
 if (myBasket) { // if it contains items diplay them
@@ -20,11 +21,10 @@ if (myBasket) { // if it contains items diplay them
     </table>
     `
     const selectTbody = document.getElementsByTagName('tbody')[0];
-    let totalPrice = 0;
     myBasket.forEach(teddy => {
         totalPrice += teddy.price * teddy.quantity / 100
         selectTbody.innerHTML += `
-            <tr>
+            <tr id="${teddy._id}">
                 <th scope="row"><i class="fas fa-minus text-info"></i> <span class="quantity">${teddy.quantity}</span> <i class="fas fa-plus text-info"></i></th>
                 <td class="w-25"><img src="${teddy.imageUrl}" class="img-fluid img-thumbnail"></td>
                 <td>${teddy.name}</td>
@@ -38,65 +38,123 @@ if (myBasket) { // if it contains items diplay them
             <th scope="row"></th>
             <td class="w-25"></td>
             <td class="total-price">Prix total</td>
-            <td class="total-price">${totalPrice} €</td>
+            <td class="total-price" id="total-price">${totalPrice} €</td>
         </tr>
         `
 } else {    // else show a message "Basket is empty"
+    showBasketIsEmptyMessage();
+}
+
+function showBasketIsEmptyMessage() {
     selectProductsDisplay.innerHTML = `
         <div class="col-12 text-center">
             <p>Le panier est vide.</p>
             <a href="index.html" class="btn btn-info">Retour à l'accueil</a>
         </div>
         `
-    selectForm.remove();
+    selectFormContainer.remove();
 }
 
 // Listen event on click for - and + to change quantity in basket
+const selectRemoveButtons = Array.from(document.getElementsByClassName('fa-minus'));
+const selectAddButtons = Array.from(document.getElementsByClassName('fa-plus'));
+const selectTotalPrice = document.getElementById('total-price');
 
-// listen event on click for order-button to send the request
-selectFormButton.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(e);
-    // validateForm();
+selectRemoveButtons.forEach(removeButton => {
+    removeButton.addEventListener('click', function () {            // listen event on every - buttons
+        const trFromArticleToRemove = this.parentNode.parentNode;   // get the tr of the article in case quantity === 0 after the operation to remove the row
+        const idFromArticleToRemove = trFromArticleToRemove.id;     // get the id of teddy to update
+        const quantityToUpdate = this.nextSibling.nextSibling;      // get quantity displayed to update it after modification
+        for (let i = 0; i < myBasket.length; i++) {
+            if (myBasket[i]._id === idFromArticleToRemove) {        // check if the article to update is the same as the one in localStorage
+                myBasket[i].quantity -= 1;                          // remove 1 from quantity
+                totalPrice -= myBasket[i].price / 100;              // update total price and refresh display
+                selectTotalPrice.textContent = `${totalPrice} €`
+                quantityToUpdate.textContent = myBasket[i].quantity;// change quantity displayed
+                if (myBasket[i].quantity === 0) {                   // if quantity now = 0 then delete the article from the array
+                    myBasket.splice(i, 1);
+                    trFromArticleToRemove.remove();                 // remove row now that quantity = 0
+                }
+                break;                                              // break the loop once it's done
+            }
+        };
+        if (myBasket.length === 0) {                                // if there is no more article in the basket show the appropriate message instead of the table
+            showBasketIsEmptyMessage();
+        }
+        localStorage.setItem('myBasket', JSON.stringify(myBasket)); // overwrite it in LocalStorage to validate the changes
+    });
 });
 
-function validateForm() {
-    // get data from form
-    const contact = {
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        address: document.getElementById("address").value,
-        city: document.getElementById("city").value,
-        email: document.getElementById("email").value
-    };
-
-    // recover articles in the basket
-    const products = [];
-    myBasket.forEach(teddy => {
-        for (let i = 0; i < teddy.quantity; i++) {
-            products.push(teddy._id)
+selectAddButtons.forEach(addButton => {
+    addButton.addEventListener('click', function () {               // listen event on every + buttons
+        const idFromArticleToAdd = this.parentNode.parentNode.id;   // get the id of teddy to update
+        const quantityToUpdate = this.previousSibling.previousSibling;  // get quantity displayed to update it after modification
+        for (let i = 0; i < myBasket.length; i++) {
+            if (myBasket[i]._id === idFromArticleToAdd) {           // check if the article to update is the same as the one in localStorage
+                myBasket[i].quantity += 1;                          // add 1 from quantity
+                totalPrice += myBasket[i].price / 100;              // update total price an refresh display
+                selectTotalPrice.textContent = `${totalPrice} €`
+                quantityToUpdate.textContent = myBasket[i].quantity;// change quantity displayed
+                break;
+            }
         }
+        localStorage.setItem('myBasket', JSON.stringify(myBasket)); // overwrite it in LocalStorage to validate the changes
     });
+});
 
-    // send request POST to backend
-    const order = JSON.stringify({
-        contact: contact,
-        products: products
-    });
+// listen event on click for order-button to send the request
+selectForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let contact = checkForm();     // check if fields in form are correct
+    // if everything is fine then send request POST to backend
+    if (contact) {
+        const products = [];      // recover articles in the basket
+        myBasket.forEach(teddy => {
+            for (let i = 0; i < teddy.quantity; i++) {
+                products.push(teddy._id)
+            }
+        });
+        const myOrder = JSON.stringify({
+            contact: contact,
+            products: products
+        });
+        sendRequestPost(myOrder);
+    } else {
+        console.error("Une erreur est survenue, veuillez ressayer plus tard.");
+    }
+});
 
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
-            localStorage.setItem("order", this.responseText);
-            window.location.href = "order.html";
-        } else if (this.readyState == XMLHttpRequest.DONE && this.status != 201) {
-            console.error("Une erreur est survenue, veuillez ressayer plus tard.");
-            return;
+function checkForm() {
+    if (document.getElementById("firstName").validity.valid &&
+        document.getElementById("lastName").validity.valid &&
+        document.getElementById("address").validity.valid &&
+        document.getElementById("city").validity.valid &&
+        document.getElementById("email").validity.valid) {
+        return {
+            firstName: document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value,
+            address: document.getElementById("address").value,
+            city: document.getElementById("city").value,
+            email: document.getElementById("email").value
+        };
+    }
+    return false;
+}
+
+function sendRequestPost(myOrder) {
+    const options = {
+        method: 'POST',
+        body: myOrder,
+        headers: {
+            'Content-Type': 'application/json'
         }
-    };
-    request.open("POST", "http://localhost:3000/api/teddies/order");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.responseType = 'text';
-    request.send(order);
-    console.log(localStorage.getItem("order"));
-};
+    }
+
+    fetch('http://localhost:3000/api/teddies/order', options)
+        .then(response => response.json())
+        .then(response => {
+            localStorage.clear();
+            localStorage.setItem("myOrder", JSON.stringify(response));
+            window.location.href = 'order.html';
+        });
+}
--- js/order.js
+++ js/order.js
@@ -1,2 +1,27 @@
 const selectOrderDisplay = document.getElementById('orderDisplay');
-const myOrder = JSON.parse(localStorage.getItem('order'));
+const myOrder = JSON.parse(localStorage.getItem('myOrder'));
+
+if (myOrder) {
+    const totalPrice = getTotalPrice();
+    selectOrderDisplay.innerHTML = `
+    <div class="col-12 text-center">
+        <p>Merci pour votre commande n° <span class="font-weight-bold">${myOrder.orderId}</span> d'un montant de <span class="font-weight-bold">${totalPrice / 100} €</span></p>
+        <a href="index.html" class="btn btn-info">Retour à l'accueil</a>
+    </div>
+    `
+} else {
+    selectOrderDisplay.innerHTML = `
+    <div class="col-12 text-center">
+        <p>Vous n'avez encore passé(e)s aucune commande</p>
+        <a href="index.html" class="btn btn-info">Retour à l'accueil</a>
+    </div>
+    `
+}
+
+function getTotalPrice() {
+    let addPrices = 0;
+    myOrder.products.forEach(product => {
+        addPrices += product.price
+    });
+    return addPrices;
+}
